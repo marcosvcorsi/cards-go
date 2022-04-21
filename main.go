124b4b966855670b1d3aecf2b004b56e1f301540
Marcos Corsi <marcosvcorsi@gmail.com>
2022-04-21 20:54:13 -0300

    feat: add deck loop

--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of spades"
 	//card := "Ace of spades"
@@ -11,16 +9,6 @@ func main() {
 
 	// fmt.Println(card)
 
-	cards := []string{newCard(), "Ace of spades"}
-	cards = append(cards, "Six of Diamonds")
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-	fmt.Println(cards)
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.print()
 }
